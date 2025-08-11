d8abfc1fb36e4a26ef2a536d834ddd21711c6c66
pawankalyancse <csepawankalyan@gmail.com>
2025-08-11 12:46:41 +0530

    Added CRUD operations to products

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,39 +1,74 @@
 const productModel = require('../models/productModel')
-const cloudinary = require('cloudinary').v2
+const cloudinary = require('../config/cloudinary')
 
-cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDINARY_API_SECRET
-})
-
-
-const addProduct = async (req, res) => {
+const addProduct = async (req, res, next) => {
     try {
         let item = req.body
-        console.log(item);
-        console.log(req.file);
+        // console.log(item);
+        // console.log(req.file);
         let result = await cloudinary.uploader.upload(req.file?.path)
-        console.log(result);
-        item = {...item, images : [result.secure_url]}
+        // console.log(result);
+        item.images = [result.secure_url]
         let product = await productModel.create(item)
         return res.status(201).send(product)
     } catch (error) {
-        return res.status(500).send({ message: error.message || "Internal Server Error" })
+        next(error)
+        // return res.status(500).send({ message: error.message || "Internal Server Error" })
     }
 }
 
-const fetchProducts = async (req, res) => {
+const fetchProduct = async (req, res, next) => {
+    try {
+        let { productId } = req.params
+        let product = await productModel.findById(productId)
+        return res.status(200).send(product)
+    } catch (error) {
+        next(error)
+        // return res.status(500).send({ message: error.message || "Internal Server Error" })
+    }
+}
+
+const fetchProducts = async (req, res, next) => {
     try {
         let products = await productModel.find({})
         return res.status(200).send(products)
     } catch (error) {
-        return res.status(500).send({ message: error.message || "Internal Server Error" })
+        next(error)
+        // return res.status(500).send({ message: error.message || "Internal Server Error" })
     }
 }
 
+const updateProduct = async (req, res, next) => {
+    try {
+        let { productId } = req.params
+        let item = req.body
+        if (req.file?.path) {
+            let result = await cloudinary.uploader.upload(req.file?.path)
+            item.images = [result.secure_url]
+        }
+        let product = await productModel.findByIdAndUpdate(productId, { $set: item }, { new: true })
+        return res.status(200).send(product)
+    } catch (error) {
+        next(error)
+        // return res.status(500).send({ message: error.message || "Internal Server Error" })
+    }
+}
+
+const deleteProduct = async (req, res, next) => {
+    try {
+        let { productId } = req.params
+        await productModel.findByIdAndDelete(productId)
+        return res.status(200).send({ message: "product deleted successfully" })
+    } catch (error) {
+        next(error)
+        // return res.status(500).send({ message: error.message || "Internal Server Error" })
+    }
+}
 
 module.exports = {
     addProduct,
-    fetchProducts
+    fetchProducts,
+    updateProduct,
+    deleteProduct,
+    fetchProduct
 }
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,22 +1,17 @@
 let express = require('express')
-let multer = require('multer')
+let upload = require('../config/multer')
 let isAuthenticated = require('../middleware/isAuthenticated')
 let isAdmin = require('../middleware/isAdmin')
-let { addProduct, fetchProducts } = require('../controllers/productsControllers')
+let { addProduct, fetchProducts, updateProduct, deleteProduct, fetchProduct } = require('../controllers/productsControllers')
 
 let productRouter = express.Router()
-let multerStorage = multer.diskStorage({
-    destination: 'uploads',
-    filename: (req, file, callBack) => {
-        callBack(null, file.originalname)
-    },
-})
-let upload = multer({ storage: multerStorage })
 
 
 productRouter.post('/', isAuthenticated, isAdmin, upload.single('image'), addProduct)
 productRouter.get('/', isAuthenticated, fetchProducts)
+productRouter.get('/:productId', isAuthenticated, fetchProduct)
+productRouter.put('/:productId', isAuthenticated, isAdmin, upload.single('image'), updateProduct)
+productRouter.delete('/:productId', isAuthenticated, isAdmin, deleteProduct)
 
 
 module.exports = productRouter
-
